cb1f7d9de1b1f3b718a2930b4694b64b59cc657f
guru <gururagavan75@gmail.com>
2024-10-18 20:27:41 +0530

    portfolio init commit

---
+++ portfoliyo/jobs/views.py
@@ -0,0 +1,12 @@
+# from urllib import request
+from django.shortcuts import render,get_object_or_404
+from .models import Job
+# Create your views here.
+def homepage(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html',{'jobs':jobs})
+
+def details(request,job_id):
+    print(job_id)
+    get_object_or_404(Job,pk=job_id)
+    return render(request,'jobs/details.html',{'job':job})
